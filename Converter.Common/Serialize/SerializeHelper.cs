c9d96c59b3c863b8ef0d33938154ece3aadf4269
mark_chen <1297552997@qq.com>
2024-05-28 10:55:56 +0800

    SerializeHelper

---
+++ Converter.Common/Serialize/SerializeHelper.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Converter.Common.Serialize;
+
+public static class SerializeHelper
+{
+    /// <summary>
+    /// Convert Object to String.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static string SerializeToString<T>(this T obj)
+    {
+        return JsonConvert.SerializeObject(obj);
+    }
+    
+    /// <summary>
+    /// Convert Object to String.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static T? SerializeToObject<T>(this string value)
+    {
+        return JsonConvert.DeserializeObject<T>(value);
+    }
+}
---
+++ Converter.Test/SerializeTest.cs
@@ -0,0 +1,30 @@
+﻿using Converter.Common.Serialize;
+
+namespace Converter.Test;
+
+public static class SerializeTest
+{
+    public static void Test()
+    {
+        var model = new SerializeTestObject
+        {
+            Id = 1,
+            Name = "Name1"
+        };
+        var str = model.SerializeToString();
+        var model1 = str.SerializeToObject<SerializeTestObject>();
+    }
+}
+
+public class SerializeTestObject
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Name { get; set; }
+}
